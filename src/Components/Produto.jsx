5e1f5ab8a66b6da84606cd93ebf6e02900c56b8c
Rogerio-Lacerda <70972583+Rogerio-Lacerda@users.noreply.github.com>
2023-07-28 16:38:15 -0300

    Criei a página de produtos onde um fetch é a realizado toda vez que entramos na página, onde ela puxa uma lista de produtos de uma api.

---
+++ src/Components/Produto.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Produto.module.css';
+
+const Produto = () => {
+  return <div>produto</div>;
+};
+
+export default Produto;
--- src/Components/Produtos.jsx
+++ src/Components/Produtos.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import styles from './Produtos.module.css';
+import useFetch from '../Hooks/useFetch';
+import { Link } from 'react-router-dom';
 
 const Produtos = () => {
-  return <div>Produtos</div>;
+  const { dados, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    request('https://ranekapi.origamid.dev/json/api/produto');
+  }, [request]);
+
+  if (loading && !dados && !error) return <div>Carregando...</div>;
+  if (error && !loading && !dados) return <p>{error}</p>;
+  if (dados && !loading && !error)
+    return (
+      <section className={styles.produtos}>
+        {dados.map(({ id, fotos, nome }) => (
+          <Link className={styles.produtosItem} key={id} to={`/produto/${id}`}>
+            <img src={fotos[0].src} alt={fotos[0].titulo} />
+            <h2>{nome}</h2>
+          </Link>
+        ))}
+      </section>
+    );
 };
 
 export default Produtos;
