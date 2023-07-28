3ff67939f9a7c7982437ab869fee4649929d29bf
Rogerio-Lacerda <70972583+Rogerio-Lacerda@users.noreply.github.com>
2023-07-28 17:35:41 -0300

    Crie a página de contato.

--- src/Components/Contato.jsx
+++ src/Components/Contato.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import styles from './Contato.module.css';
+import foto from '../assets/contato.jpg';
 
 const Contato = () => {
-  return <div>Contato</div>;
+  return (
+    <section className={styles.contato}>
+      <img src={foto} alt="Máquina de escrever" />
+      <div className={styles.conteudo}>
+        <h2>Entre em Contato.</h2>
+        <ul className={styles.informacoes}>
+          <li>produto@gmail.com</li>
+          <li>99999-9999</li>
+          <li>Rua Ali Perto, 999</li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Contato;
